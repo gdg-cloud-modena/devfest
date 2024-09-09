81af91bcd64286e1352121ec533fa0db37e7d2f2
Carmine Zaccagnino <carmine@carminezacc.com>
2024-09-09 18:00:19 +0200

    eventbrite checkout
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- nuxt-website/nuxt.config.ts
+++ nuxt-website/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [{src: 'https://www.eventbrite.it/static/widgets/eb_widgets.js'}]
+    }
+  }
 })
