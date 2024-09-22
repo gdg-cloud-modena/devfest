0fb5211b5c6fdd989c20b9fdb7349faf78b54f43
Grimos10 <biagiogrimos@gmail.com>
2024-09-22 12:28:19 +0200

    Refactor secondary links and add new pages
    - Refactored the secondary links component in secondary-links.vue to remove the "Codice di condotta" link and add a new "Sponsor" link.
    - Created new pages for code of conduct, legal, privacy policy, and sponsors.
    
    Closes #15
    Closes #14

--- nuxt-website/components/secondary-links.vue
+++ nuxt-website/components/secondary-links.vue
@@ -2,15 +2,16 @@
     <div class="flexbox column" style="gap: 12px">
         <div class="flexbox wrap pad-24" style="gap: 48px;">
             <h4><RouterLink to="/faq">FAQ</RouterLink></h4>
-            <h4><RouterLink to="/contact-us">Contacts</RouterLink></h4>
             <h4><RouterLink to="/location">Location</RouterLink></h4>
+            <h4><RouterLink to="/sponsors">Sponsor</RouterLink></h4>
             <h4><RouterLink to="/team">Team</RouterLink></h4>
             <h4><RouterLink to="/communities">Comunità</RouterLink></h4>
-            <h4>Codice di condotta</h4>
+            <h4><RouterLink to="/contact-us">Contacts</RouterLink></h4>
+            <h4><RouterLink to="/code-of-conduct">Codice di condotta</RouterLink></h4>
         </div>
         <div class="flexbox wrap pad-24" style="gap: 48px;">
-            <p>Privacy policy</p>
-            <p>Note legali</p>
+            <p><RouterLink to="/privacy">Privacy Policy</RouterLink></p>
+            <p><RouterLink to="/legal">Note legali</RouterLink></p>
         </div>
     </div>
 </template>
---
+++ nuxt-website/pages/sponsors.vue
@@ -0,0 +1,32 @@
+<template>
+    <Title>
+        DevFest Modena 2024 &mdash; Sponsors
+    </Title>
+    <div>
+        <NuxtLayout name="page">
+            <template #image>
+                <div style="max-width: 100px; max-height: 200px;">
+                    <img style="max-width: 100%; max-height: 100%;" src="@/assets/svg/Plus.svg">
+                </div>
+            </template>
+            <template #title>
+                I nostri sponsor
+            </template>
+            <template #content>
+                <div style="text-align: center;">
+                    <h1>PAGINA DA CREARE</h1>
+                </div>
+            </template>
+            <template #under-content>
+                <FreeTicket />
+                <SocialLinks />
+            </template>
+        </NuxtLayout>
+    </div>
+</template>
+
+<script setup lang="ts">
+    definePageMeta({
+        layout: false,
+    });
+</script>
