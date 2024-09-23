dd2e33b640bd10f55800ad84183daecf80be8654
Grimos10 <biagiogrimos@gmail.com>
2024-09-23 10:29:59 +0200

    Refactor community cards.
    Issue #22 done, waiting for Reggio Emilia Full Stack logo

--- nuxt-website/pages/communities.vue
+++ nuxt-website/pages/communities.vue
@@ -23,60 +23,42 @@
                     </div>
                 </div>
                 <div class="flexbox wrap justify-center align-top" style="gap: 48px; flex: 1 1 400px;">
-                    <div class="card-partner">
-                        <div class="image">
-                            <img src="@/assets/img/Logo(1).png" />
+                    <a href="https://gdg.community.dev/gdg-cloud-modena/">
+                        <div class="white-card team-card">
+                            <div class="image image-staff gdg-modena"></div>
+                            <h4>GDG Cloud Modena</h4>
                         </div>
-                        <p>ConoscereLinux</p>
-                        <div class="image-bottom">
-                            <a href="https://conoscerelinux.org/"><img src="@/assets/img/ic_open_in_new.png" /></a>
+                    </a>
+                    <a href="https://www.meetup.com/it-IT/Modena-Full-Stack/">
+                        <div class="white-card team-card">
+                            <div class="image image-staff modena-full-stack"></div>
+                            <h4>Modena Full Stack</h4>
                         </div>
-                    </div>
-                    <div class="card-partner">
-                        <div class="image">
-                            <img src="@/assets/img/Logo(2).png" />
+                    </a>
+                    <a href="https://blog.fluttermodena.dev/">
+                        <div class="white-card team-card">
+                            <div class="image image-staff flutter-modena"></div>
+                            <h4>Flutter Modena</h4>
                         </div>
-                        <p>Flutter Modena</p>
-                        <div class="image-bottom">
-                            <a href="https://blog.fluttermodena.dev/"><img src="@/assets/img/ic_open_in_new.png" /></a>
+                    </a>
+                    <a href="https://conoscerelinux.org/">
+                        <div class="white-card team-card">
+                            <div class="image image-staff conoscere-linux"></div>
+                            <h4>ConoscereLinux</h4>
                         </div>
-                    </div>
-                    <div class="card-partner">
-                        <div class="image">
-                            <img src="@/assets/img/Logo(3).png" />
+                    </a>
+                    <a href="https://mlmodena.it/">
+                        <div class="white-card team-card">
+                            <div class="image image-staff ml-modena"></div>
+                            <h4>ML Modena</h4>
                         </div>
-                        <p>GDG Cloud Modena</p>
-                        <div class="image-bottom">
-                            <a href="https://gdg.community.dev/gdg-cloud-modena/"><img src="@/assets/img/ic_open_in_new.png" /></a>
+                    </a>
+                    <a href="https://gdg.community.dev/gdg-bologna/">
+                        <div class="white-card team-card">
+                            <div class="image image-staff gdg-bologna"></div>
+                            <h4>GDG Bologna</h4>
                         </div>
-                    </div>
-                    <div class="card-partner">
-                        <div class="image">
-                            <img src="@/assets/img/Logo(4).png" />
-                        </div>
-                        <p>ML Modena</p>
-                        <div class="image-bottom">
-                            <a href="https://mlmodena.it/"><img src="@/assets/img/ic_open_in_new.png" /></a>
-                        </div>
-                    </div>
-                    <div class="card-partner">
-                        <div class="image">
-                            <img src="@/assets/img/Logo(5).png" />
-                        </div>
-                        <p>Modena Full Stack</p>
-                        <div class="image-bottom">
-                            <a href="https://www.meetup.com/it-IT/Modena-Full-Stack/"><img src="@/assets/img/ic_open_in_new.png" /></a>
-                        </div>
-                    </div>
-                    <div class="card-partner">
-                        <div class="image">
-                            <img src="@/assets/img/Logo(6).png" />
-                        </div>
-                        <p>GDG Bologna</p>
-                        <div class="image-bottom">
-                            <a href="https://gdg.community.dev/gdg-bologna/"><img src="@/assets/img/ic_open_in_new.png" /></a>
-                        </div>
-                    </div>
+                    </a>
                 </div>
             </template>
             <template #under-content>
