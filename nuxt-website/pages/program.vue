a3852c34330f9e996dfc539eec01aa364fc420cc
Grimos10 <biagiogrimos@gmail.com>
2024-09-24 16:53:42 +0200

    Refactor program page layout and add timetable cards, not finished

--- nuxt-website/pages/program.vue
+++ nuxt-website/pages/program.vue
@@ -12,46 +12,54 @@
             <template #title>
                 Programma da definire
             </template>
-            <!--template #content>
-                <div class="swapper-500">
+            <template #content>
+                <div class="swapper-500" style="padding-top: 48px;">
                     <div class="main">
                         <TimetableCard 
+                        timetable-type="break" 
+                        time="9:00 - 9:30"
+                        speaker=""
+                        title="Inizio dell'evento, accoglienza e networking"
+                        class="above border-bottom"
+                        />
+                        <TimetableCard 
                             timetable-type="break" 
-                            time="10:30 - 11:00"
-                            speaker="Giancarlo De Bongleris"
-                            title="Go è terribile"
-                            room-and-language="Sala Grande - Italiano"
-                            url="/speakers"
-                            class="above border-bottom"
+                            time="9:30 - 9:55"
+                            speaker=""
+                            title="Intro/Keynote"
+                            room-and-language="Sala 1 & 2 - Italiano"
+                            class="middle border-bottom"
                         />
                         <div class="flexbox">
                             <TimetableCard 
                                 timetable-type="event" 
-                                time="10:30 - 11:00"
-                                speaker="Ponglo Ponglers"
-                                title="Rust è figo"
+                                time="AM - AM"
+                                speaker="Riccardo Carlesso"
+                                title="News Crawler via Langchain.RB and Gemini APIs"
                                 room-and-language="Sala 1 - Italiano"
                                 url="/speakers"
                                 style="flex: 1"
-                            />
-                            <TimetableCard 
-                                timetable-type="event" 
-                                time="10:30 - 11:00"
-                                speaker="Ponglo Ponglers"
-                                title="Rust è figo"
-                                room-and-language="Sala 1 - Italiano"
-                                url="/speakers"
                                 class="border"
-                                style="flex: 1"
                             />
                             <TimetableCard 
                                 timetable-type="event" 
-                                time="10:30 - 11:00"
-                                speaker="Ponglo Ponglers"
-                                title="Rust è figo"
-                                room-and-language="Sala 1 - Italiano"
+                                time="AM - AM"
+                                speaker="Coderdojo"
+                                title="Intro/Keynote"
+                                room-and-language="Sala Workshop - Italiano"
                                 url="/speakers"
                                 style="flex: 1"
+                            />
+                            
+                            <TimetableCard 
+                                timetable-type="event" 
+                                time="AM - AM"
+                                speaker="Francesco Baldassarri "
+                                title="Unleash the Power of Llama 3.1 on Your Local Hardware – No Cloud Needed!"
+                                room-and-language="Sala 2 - Italiano"
+                                url="/speakers"
+                                style="flex: 1"
+                                class="border"
                             />
                         </div>
                         <TimetableCard 
@@ -105,7 +113,7 @@
                         </div>
                     </div>
                 </div>
-            </template-->
+            </template>
             <template #under-content>
                 <FreeTicket />
                 <SocialLinks />
