ca90560f67039b02b838df57e17a749be1b3f19a
Davide Cavallini <dcavallinister@gmail.com>
2024-10-02 11:07:04 +0200

    add "patrocinato da comune modena"

--- nuxt-website/components/secondary-links.vue
+++ nuxt-website/components/secondary-links.vue
@@ -1,20 +1,46 @@
 <template>
-    <div class="flexbox column w-full">
-        <div class="flexbox wrap pad-24 gap-4 lg:gap-8">
-            <div class="text-lg"><RouterLink to="/faq">FAQ</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/location">Location</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/sponsors">Sponsor</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/team">Team</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/communities">Communities</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/contact-us">Contacts</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/code-of-conduct">Codice di condotta</RouterLink></div>
-            <div class="text-lg"><RouterLink to="/program">Programma</RouterLink></div>
-        </div>
-        <div class="flexbox wrap pad-24 gap-4 lg:gap-8">
-            <RouterLink to="/privacy">Privacy Policy</RouterLink>
-            <RouterLink to="/legal">Note legali</RouterLink>
-        </div>
+  <div class="flexbox column w-full">
+    <div class="flexbox wrap pad-24 gap-4 lg:gap-8">
+      <div class="text-lg">
+        <RouterLink to="/faq">FAQ</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/location">Location</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/sponsors">Sponsor</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/team">Team</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/communities">Communities</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/contact-us">Contacts</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/code-of-conduct">Codice di condotta</RouterLink>
+      </div>
+      <div class="text-lg">
+        <RouterLink to="/program">Programma</RouterLink>
+      </div>
     </div>
+    <div class="flex flex-col lg:flex-row lg:justify-between  pad-24">
+      <div class="flexbox wrap gap-4 lg:gap-8">
+        <RouterLink to="/privacy">Privacy Policy</RouterLink>
+        <RouterLink to="/legal">Note legali</RouterLink>
+      </div>
+      <div class="flex item-center mt-8 lg:mt-0">
+        <div>
+          Patrocinato da
+        </div>
+        <div>
+          <img class="ml-3" style="height: 25px" src="@/assets/img/comune-modena.jpeg"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
