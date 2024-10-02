bcdd9934855b121fda90d35574badfe97f0c6dfc
Davide Cavallini <dcavallinister@gmail.com>
2024-10-02 08:49:37 +0200

    fix eventbrite section

--- nuxt-website/components/ticket-widget.vue
+++ nuxt-website/components/ticket-widget.vue
@@ -1,37 +1,37 @@
 <template>
-    <section 
-            id="ticket"
-            class="flexbox column pad-24 full"
-            style="gap: 48px;"
-        >   
-        <div class="flexbox justify-between column">
-            <h1 class="text-accent">Biglietti</h1>
-            <h4>
-                Ti interessa? Prendi il tuo biglietto su Eventbrite. L’ingresso è gratuito.
-            </h4>
-        </div>
-        <div id="eventbrite-widget-container-1006887817567" style="flex: 1;"></div>
-    </section>
+  <section
+      id="ticket"
+      class="flexbox column pad-24 full"
+      style="gap: 48px;"
+  >
+    <div class="flexbox justify-between column">
+      <div class="text-accent text-4xl lg:text-6xl font-bold">Biglietti</div>
+      <div class="text-lg lg:text-2xl pt-6">
+        Ti interessa? Prendi il tuo biglietto su Eventbrite. L’ingresso è gratuito.
+      </div>
+    </div>
+    <div id="eventbrite-widget-container-1006887817567" style="flex: 1;"></div>
+  </section>
 </template>
 <script setup>
 onMounted(async () => {
-    var exampleCallback = function() {
-            console.log('Ordine completato.');
-        };
-        console.log("Creating widget");
+  var exampleCallback = function () {
+    console.log('Ordine completato.');
+  };
+  console.log("Creating widget");
 
-        window.EBWidgets.createWidget({
-            // Required
-            widgetType: 'checkout',
-            eventId: '1006887817567',
-            iframeContainerId: 'eventbrite-widget-container-1006887817567',
+  window.EBWidgets.createWidget({
+    // Required
+    widgetType: 'checkout',
+    eventId: '1006887817567',
+    iframeContainerId: 'eventbrite-widget-container-1006887817567',
 
-            // Optional
-            iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
-            onOrderComplete: exampleCallback  // Method called when an order has successfully completed
-        });
+    // Optional
+    iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
+    onOrderComplete: exampleCallback  // Method called when an order has successfully completed
+  });
 
-        console.log("Created widget");
+  console.log("Created widget");
 
 });
 </script>
