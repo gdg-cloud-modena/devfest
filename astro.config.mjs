64e806103f918f23c45c90f5af82687a76315f73
Marco Pollacci <m.pollacci@40-factory.com>
2026-03-27 08:36:03 +0100

    feat(project): :sparkles: Adding sitemaps configuration on astro ( ref #59)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 // @ts-check
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
+
+import { SITE_URL } from "./src/config/site";
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/en/"),
+    }),
+    robotsTxt({
+      sitemap: [`${SITE_URL}sitemap-index.xml`, `${SITE_URL}en/sitemap.xml`],
+    }),
+  ],
   vite: {
     build: {
       rollupOptions: {
---
+++ src/pages/en/sitemap.xml.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from "astro";
+import { SITE_URL } from "../../config/site";
+
+export const GET: APIRoute = async () => {
+  const pages = [`${SITE_URL}en/`];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages.map((url) => `<url><loc>${url}</loc></url>`).join("")}
+    </urlset>`.trim();
+
+  return new Response(sitemap, {
+    headers: { "Content-Type": "application/xml" },
+  });
+};
